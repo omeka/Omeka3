3becf2fd01cd71b7843b0e4f5369c6746b6b55ef
Daniel Berthereau <Daniel.github@Berthereau.net>
2017-12-18 00:00:00 +0100

    Fixed gulp modules paths.

--- gulpfile.js
+++ gulpfile.js
@@ -364,6 +364,7 @@ gulp.task('deps:js', taskDepsJs);
 
 gulp.task('deps:module:npm', function (done) {
     var modulePath = path.join(__dirname, 'modules', cliOptions.module);
+    process.chdir(modulePath);
     var manifest = path.join(modulePath, 'package.json');
     return fs.existsSync(manifest)
         ? runCommand('npm', ['install'], {cwd: modulePath})
@@ -372,6 +373,7 @@ gulp.task('deps:module:npm', function (done) {
 
 gulp.task('deps:module:gulp', function (done) {
     var modulePath = path.join(__dirname, 'modules', cliOptions.module);
+    process.chdir(modulePath);
     var manifest = path.join(modulePath, 'gulpfile.js');
     return fs.existsSync(manifest)
         ? runCommand('gulp', ['install'], {cwd: modulePath})
@@ -380,6 +382,7 @@ gulp.task('deps:module:gulp', function (done) {
 
 gulp.task('deps:module:composer', function (done) {
     var modulePath = path.join(__dirname, 'modules', cliOptions.module);
+    process.chdir(modulePath);
     var manifest = path.join(modulePath, 'composer.json');
     return fs.existsSync(manifest)
         ? composer(['install'])
@@ -394,6 +397,7 @@ gulp.task('deps:module', gulp.series(
 
 gulp.task('deps:module:npm:update', function (done) {
     var modulePath = path.join(__dirname, 'modules', cliOptions.module);
+    process.chdir(modulePath);
     var manifest = path.join(modulePath, 'package.json');
     return fs.existsSync(manifest)
         ? runCommand('npm', ['update'], {cwd: modulePath})
@@ -402,6 +406,7 @@ gulp.task('deps:module:npm:update', function (done) {
 
 gulp.task('deps:module:gulp:update', function (done) {
     var modulePath = path.join(__dirname, 'modules', cliOptions.module);
+    process.chdir(modulePath);
     var manifest = path.join(modulePath, 'gulpfile.js');
     return fs.existsSync(manifest)
         ? runCommand('gulp', ['update'], {cwd: modulePath})
@@ -410,6 +415,7 @@ gulp.task('deps:module:gulp:update', function (done) {
 
 gulp.task('deps:module:composer:update', function (done) {
     var modulePath = path.join(__dirname, 'modules', cliOptions.module);
+    process.chdir(modulePath);
     var manifest = path.join(modulePath, 'composer.json');
     return fs.existsSync(manifest)
         ? composer(['update'])

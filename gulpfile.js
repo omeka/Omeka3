746fe5326d3b1d9f411f6fc7d0dc6b49c3c135bd
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-12-03 00:00:00 +0100

    Returned a callback to the gulp zip task.

--- gulpfile.js
+++ gulpfile.js
@@ -203,7 +203,7 @@ function phpCsFixer(fix, modulePath) {
     });
 }
 
-function zipDistDir(source = null, destination = null, zipname = null) {
+function zipDistDir(source = null, destination = null, zipname = null, done) {
     if (!source) source = '.';
     if (!destination) destination = buildDir;
     if (!zipname) zipname = source.replace( /\\/g, '/' ).replace( /.*\//, '' );
@@ -223,7 +223,8 @@ function zipDistDir(source = null, destination = null, zipname = null) {
         '!./**/.gitattributes',
         '!./**/.gitignore'
     ];
-    return gulp.src(
+
+    var stream = gulp.src(
             files,
             {base: source, nodir: true, dot: true}
         )
@@ -232,6 +233,8 @@ function zipDistDir(source = null, destination = null, zipname = null) {
         }))
         .pipe(zip(zipname + '.zip'))
         .pipe(gulp.dest(destination));
+
+    stream.on('end', done);
 }
 
 function taskCss() {
@@ -617,8 +620,8 @@ gulp.task('clean:module', function () {
     });
 });
 
-var taskZip = gulp.series('clean', 'init', function () {
-    return zipDistDir(__dirname, buildDir, 'omeka-s');
+var taskZip = gulp.series('clean', 'init', function (done) {
+    return zipDistDir(__dirname, buildDir, 'omeka-s', done);
 });
 taskZip.description = 'Create zip archive'
 gulp.task('zip', taskZip);
@@ -632,11 +635,11 @@ gulp.task('zip:module', gulp.series(
     },
     'clean:module',
     'init:module',
-    function () {
+    function (done) {
         var module = cliOptions.module;
         var modulePathPromise = getModulePath(module);
-        return modulePathPromise.then(function (modulePath) {
-            return zipDistDir(modulePath, path.join(modulePath, 'build'))
+        modulePathPromise.then(function (modulePath) {
+            zipDistDir(modulePath, path.join(modulePath, 'build'), null, done);
         });
     }
 ));
